1c456b23a6580f96b96319f96a5d92e0e46c7e5d
Lutfykumar <lutfhiesita@gmail.com>
2026-06-27 01:57:29 +0700

    chore: add .env support with godotenv for backend config
    - Add godotenv to load .env file automatically
    - Create .env.example with default values
    - Update .gitignore to exclude .env files

--- backend/config/config.go
+++ backend/config/config.go
@@ -2,27 +2,31 @@ package config
 
 import (
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Port        string
-	DBHost      string
-	DBPort      string
-	DBUser      string
-	DBPassword  string
-	DBName      string
-	DBSSLMode   string
+	Port       string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
 }
 
 func Load() *Config {
+	godotenv.Load()
+
 	return &Config{
-		Port:        getEnv("PORT", "8080"),
-		DBHost:      getEnv("DB_HOST", "localhost"),
-		DBPort:      getEnv("DB_PORT", "5432"),
-		DBUser:      getEnv("DB_USER", "postgres"),
-		DBPassword:  getEnv("DB_PASSWORD", "postgres"),
-		DBName:      getEnv("DB_NAME", "task_tracker"),
-		DBSSLMode:   getEnv("DB_SSL_MODE", "disable"),
+		Port:       getEnv("PORT", "8080"),
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "task_tracker"),
+		DBSSLMode:  getEnv("DB_SSL_MODE", "disable"),
 	}
 }
 
